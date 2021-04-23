1d43974f087da55dfece981b87ef24e1a185d2b6
Ethan Rafael <winsoftheepic@gmail.com>
2021-04-22 20:50:11 -0700

    Planet spins on the menu too

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -7,32 +7,34 @@ class Menu extends Phaser.Scene
 
     preload()
     {
-        /*this.load.spritesheet("titleBG", "./assets/title.png", 
+        this.load.spritesheet("titleBG", "./assets/spritesheets/spinning_planet.png", 
         {   
-            frameWidth: 640,
-            frameHeight: 480,
+            frameWidth: 1280,
+            frameHeight: 720,
             startFrame: 1,
-            endFrame: 59,
-        });*/
+            endFrame: 120,
+        });
         console.log("Enter Menu Scene");
     }
 
     create()
     {
-
-        /*this.anims.create(
+        this.backgroundAnim = this.add.sprite(0, 0, "titleBG").setOrigin(0,0);
+        this.anims.create(
             {
                 key: "title", 
                 frames: this.anims.generateFrameNumbers("titleBG", 
                 {
                     start: 1,
-                    end: 59,
+                    end: 119,
                     nextAnim: "title",
                 }),
-                frameRate: 10,
-            });*/
+                frameRate: 170,
+            });
 
-        this.menuText = this.add.text(game.config.width/2, game.config.height/2, 'Left/Right arrow to play scene')
+        this.menuText = this.add.text(game.config.width/2, game.config.height/2, 'Left/Right arrow to play scene');
+        this.backgroundAnim.anims.play("title");
+        this.backgroundAnim.anims.setRepeat(-1);
 
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
     }
