15f2e4adbfd84dfe0bc183d8873514975e2c9e09
Sky Peterson <katherine.ev.peterson@gmail.com>
2021-05-06 23:23:55 -0700

    Created jump mechanic.

---
+++ src/prefabs/Player.js
@@ -0,0 +1,80 @@
+class Player extends Phaser.Physics.Arcade.Sprite{
+    constructor(scene, x, y){
+        super(scene, x, y, 'cycle');
+        // setting up animations
+        scene.anims.create({
+            key: 'idle',
+            frames: this.anims.generateFrameNames('cycle',
+            {
+                prefix: "Idle",
+                start: 1,
+                end: 4,
+                zeroPad: 4,
+            }),
+            frameRate: 15,
+            repeat: -1
+        });
+        scene.anims.create({
+            key: 'jump',
+            frames: this.anims.generateFrameNames('cycle',
+            {
+                prefix: "Jump",
+                start: 1,
+                end: 4,
+                zeroPad: 4,
+            }),
+            frameRate: 15
+        });
+        scene.anims.create({
+            key: 'jumphold',
+            frames: this.anims.generateFrameNames('cycle', 
+            {
+                prefix: "jumploop",
+                start: 1,
+                end: 4,
+                zeroPad: 4,
+            }),
+            frameRate: 15
+        });
+        this.play('idle');
+        // adding sprite and physics object to scene
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
+        this.setMaxVelocity(0, MAX_VEL);
+        // setting up variables
+        this.isJumping = false;
+        this.onGround = true;
+        this.jumps = 1;
+
+        //
+        let MAX_VEL = 2000;
+        let JUMP_VEL = -600;
+        let MAX_JUMPS = 1;
+        // currently for debugging purposes
+        this.setCollideWorldBounds(true);
+    }
+    
+    update() {
+        // checking if player is on the ground
+        this.onGround = this.body.touching.down;
+        //if player on ground, and jump button held, jump
+        if(Phaser.Input.Keyboard.DownDuration(keyUP, 150) && !this.isJumping
+           && this.jumps > 0){
+            this.isJumping = true;
+            this.play('jump',true);
+            this.body.velocity.y = JUMP_VEL;
+        }
+        //if player in midair
+        if(!this.onGround && this.isJumping){
+            this.play('jumphold',true);
+            this.isJumping = false;
+            this.jumps--;
+        }
+        // if player has landed, go back to idle sprite, reset number of jumps
+        // NOTE: currently broken due to lack of item to collide with
+        if(!this.isJumping && this.onGround){
+            this.play('idle',true);
+            this.jumps = MAX_JUMPS;
+        }
+    }
+}
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -9,6 +9,7 @@ class Play extends Phaser.Scene
     {
         this.load.atlas("planet_sheet", "./assets/spritesheets/spinning_planet.png", "./assets/spritesheets/spinning_planet.json");
         this.load.image("gridBG", "./assets/single_sprites/grid_bg.png");
+        this.load.atlas("cycle", "./assets/spritesheets/Player_atlas.png", "./assets/spritesheets/Player_Atlas.json");
     }
 
     create()
@@ -27,8 +28,25 @@ class Play extends Phaser.Scene
                 }),
                 frameRate: 30,
             });
-            this.backgroundAnim.play("spinning_planet_anim");
-            this.backgroundAnim.anims.setRepeat(-1);
+        this.backgroundAnim.play("spinning_planet_anim");
+        this.backgroundAnim.anims.setRepeat(-1);
+
+        // gravity in pixels/sec
+        this.physics.world.gravity.y = 2600;
+        // creating 🏍
+        this.player = new Player(this,game.config.width/3, game.config.height/2);
+
+        // defining key
+        keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+
+        // create ground object here
+        let testObj = this.add.rectangle(0, this.game.config.height-game.config.height/15, game.config.width, game.config.height/15, 0x000000);
+        this.physics.add.existing(testObj);
+        testObj.body.immovable = true;
+        testObj.body.allowGravity = false;
+        
+        //create collider w/ ground
+        this.physics.add.collider(this.player, this.testObj);
     }
 
     update(time, delta)
@@ -39,5 +57,6 @@ class Play extends Phaser.Scene
         That way they don't speed up on high refresh rate displays. Ask Ethan for more help/info
         if you are unsure.
         */
+       this.player.update();
     }
 }
