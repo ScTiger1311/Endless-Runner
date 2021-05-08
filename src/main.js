0d53bad476b061a184fa24d6a2d8dfad51099bfc
mrzebrasmoothie <averyweibel@yahoo.com>
2021-05-08 03:48:37 -0700

    Comments

--- src/main.js
+++ src/main.js
@@ -6,10 +6,14 @@
 // Creative tilt justification: 
 // Technically interesting things: the obstacles move along a path and 
 //                                 get occluded by masks to give the illusion of orbiting the planet.
+//  Some small things to achieve the effect were done on lines 100-105 in basicObstacle.js (And somewhere in the other one)
+//  To scale the obstacles down as the went around the back to help sell the effect
+//  A major part of the effect was learning how to use and manipulate path followers to get the obstacles moving in the desired way
 //
 // Style: A slick mixture of 3D polygonal art, and 2d pixel art.
 // Very fancy and cool synthwave aesthetic, with a soundtrack that's better than I thought it would be.
 // I think we're all proud of how the game turned out stylistically
+
 
 let config =
 {
